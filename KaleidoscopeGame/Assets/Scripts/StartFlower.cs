9df91e0312e40fa8579eade173dd24afdbcffe94
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-12-15 04:20:00 +0900

    タイトル画面作成

---
+++ KaleidoscopeGame/Assets/Scripts/StartFlower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFlower : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float speed;
+    private int num = 0;
+    private UnityEngine.UI.Image image;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        image = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    void Start()
+    {
+        SpriteAnimation();
+    }
+
+    private void SpriteAnimation()
+    {
+        StartCoroutine(SwitchSprite());
+    }
+
+    private IEnumerator SwitchSprite()
+    {
+        image.sprite = sprites[num];
+        yield return new WaitForSeconds(speed);
+        if (num < sprites.Length - 1) num++;
+        else num = 0;
+        SpriteAnimation();
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ KaleidoscopeGame/Assets/Scripts/TitleCtrl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleCtrl : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeSpeed;
+    private bool startGame = false;
+    [SerializeField]private int fadeNum = 0;
+    private float alpha = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fadeImage.color = new Color(255, 255, 255, alpha);
+        FadeAction();
+        KeyCtrl();
+    }
+
+    private void KeyCtrl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mouseDown");
+            if (startGame) return;
+            StartCoroutine(StartGame());
+        }
+    }
+
+    private void FadeAction()
+    {
+        switch (fadeNum)
+        {
+            case 1:
+                if(alpha < 1)
+                {
+                    alpha += Time.deltaTime * fadeSpeed;
+                }
+                else
+                {
+                    alpha = 1;
+                    fadeNum = 0;
+                }
+                break;
+        }
+    }
+    
+    private IEnumerator StartGame()
+    {
+        if (startGame) yield break;
+        startGame = true;
+        fadeNum = 1;
+        while(fadeNum != 0)
+        {
+            yield return null;
+        }
+        SceneManager.LoadSceneAsync(nextSceneName);
+  
+    }
+}
